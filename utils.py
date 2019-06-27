0a445be062d158abcba78b35bbac5fcdc0acc2b5
Duncan Campbell <duncan@donut-hole.fios-router.home>
2019-06-27 14:59:23 -0400

    updated readme and fixed doc string

--- utils.py
+++ utils.py
@@ -13,7 +13,7 @@ __author__ = ['Duncan Campbell']
 
 def sample_ellipsoidal_surface(npts, semi_axes):
     r"""
-    return points sampled uniformly on the surface of an ellipsoidal surface
+    return points sampled uniformly on the surface of a hyperellipsoid
 
     Parameters
     ----------
@@ -32,6 +32,9 @@ def sample_ellipsoidal_surface(npts, semi_axes):
     semi_axes = np.sort(np.atleast_1d(semi_axes))
     ndim = len(semi_axes)
 
+    if ndim <= 1:
+        raise ValueError('hyperellipsoid dimension must be >=2.')
+
     x = np.random.normal(size=(npts,ndim), scale=semi_axes)
     r = np.sqrt(np.sum((x/semi_axes)**2, axis=-1))
 
@@ -40,7 +43,7 @@ def sample_ellipsoidal_surface(npts, semi_axes):
 
 def sample_ellipsoidal_volume(npts, semi_axes):
     r"""
-    return points sampled uniformly within an ellipsoidal volume
+    return points sampled uniformly within a hyperellipsoidal volume
 
     Parameters
     ----------
@@ -58,6 +61,9 @@ def sample_ellipsoidal_volume(npts, semi_axes):
 
     semi_axes = np.sort(np.atleast_1d(semi_axes))[::-1]
     ndim = len(semi_axes)
+
+    if ndim <= 1:
+        raise ValueError('hyperellipsoid dimension must be >=2.')
 
     x = np.random.normal(size=(npts,ndim), scale=semi_axes)
     r = np.sqrt(np.sum((x/semi_axes)**2, axis=-1))
