4a5b57f1104277f6ec10245e3a3d7cf375785aa1
Duncan Campbell <duncan.campbell@yale.edu>
2019-07-01 16:55:59 -0400

    added unit test for weights argument

--- tests/test_inertia_tensors.py
+++ tests/test_inertia_tensors.py
@@ -5,7 +5,7 @@ from inertia_tensors.inertia_tensors import inertia_tensors
 from inertia_tensors.utils import sample_ellipsoidal_volume
 
 
-__all__ = ('test_1',)
+__all__ = ('test_1', 'test_2', 'test_3')
 
 
 def test_1():
@@ -17,12 +17,12 @@ def test_1():
     n2 = 100
     ndim = 2
 
-    semi_axes = np.random.random((n1,ndim))
-    coords = [sample_ellipsoidal_volume(n2, semi_axes[i]) for i in range(0,n1)]
+    semi_axes = np.random.random((n1, ndim))
+    coords = [sample_ellipsoidal_volume(n2, semi_axes[i]) for i in range(0, n1)]
 
     Is = inertia_tensors(coords)
 
-    assert np.shape(Is)==(n1,ndim,ndim)
+    assert np.shape(Is) == (n1, ndim, ndim)
 
 
 def test_2():
@@ -34,9 +34,31 @@ def test_2():
     n2 = 100
     ndim = 3
 
-    semi_axes = np.random.random((n1,ndim))
-    coords = [sample_ellipsoidal_volume(n2, semi_axes[i]) for i in range(0,n1)]
+    semi_axes = np.random.random((n1, ndim))
+    coords = [sample_ellipsoidal_volume(n2, semi_axes[i]) for i in range(0, n1)]
 
     Is = inertia_tensors(coords)
 
-    assert np.shape(Is)==(n1,ndim,ndim)
+    assert np.shape(Is) == (n1, ndim, ndim)
+
+
+def test_3():
+    """
+    test weights argument
+    """
+
+    n1 = 10
+    n2 = 100
+    ndim = 3
+
+    semi_axes = np.random.random((n1, ndim))
+    coords = [sample_ellipsoidal_volume(n2, semi_axes[i]) for i in range(0, n1)]
+
+    weights = np.ones((n1, n2))
+
+    Is_1 = inertia_tensors(coords, weights=weights)
+    Is_2 = inertia_tensors(coords, weights=None)
+
+    assert np.shape(Is_1) == (n1, ndim, ndim)
+    assert np.shape(Is_2) == (n1, ndim, ndim)
+    assert np.allclose(Is_1, Is_2)
--- tests/test_iterative_inertia_tensors.py
+++ tests/test_iterative_inertia_tensors.py
@@ -8,7 +8,7 @@ from inertia_tensors.utils import sample_ellipsoidal_volume
 __all__ = ('test_1',)
 
 
-def test_2():
+def test_1():
     """
     basic 3-d unit test
     """
@@ -17,9 +17,32 @@ def test_2():
     n2 = 100
     ndim = 3
 
-    semi_axes = np.random.random((n1,ndim))
-    coords = np.array([sample_ellipsoidal_volume(n2, semi_axes[i]) for i in range(0,n1)])
+    semi_axes = np.random.random((n1, ndim))
+    coords = np.array([sample_ellipsoidal_volume(n2, semi_axes[i]) for i in range(0, n1)])
 
     Is = iterative_inertia_tensors_3D(coords)
 
-    assert np.shape(Is)==(n1,ndim,ndim)
+    assert np.shape(Is) == (n1, ndim, ndim)
+
+
+def test_2():
+    """
+    test weights
+    """
+
+    n1 = 10
+    n2 = 100
+    ndim = 3
+
+    semi_axes = np.random.random((n1, ndim))
+    coords = np.array([sample_ellipsoidal_volume(n2, semi_axes[i]) for i in range(0, n1)])
+
+    weights = np.ones((n1, n2))
+
+    Is_1 = iterative_inertia_tensors_3D(coords, weights=weights)
+    Is_2 = iterative_inertia_tensors_3D(coords, weights=None)
+
+    assert np.shape(Is_1) == (n1, ndim, ndim)
+    assert np.shape(Is_2) == (n1, ndim, ndim)
+    assert np.allclose(Is_1, Is_2)
+
